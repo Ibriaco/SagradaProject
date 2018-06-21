f6c505fbce1840727429791e74991576adfda9f3
Gregorio <gregorio.galletti@mail.polimi.it>
2018-06-21 23:13:18 +0200

    GUI updated, now all the window cards are shown

--- src/main/java/it/polimi/se2018/view/ui/guicontrollers/GUIChoiceController.java
+++ src/main/java/it/polimi/se2018/view/ui/guicontrollers/GUIChoiceController.java
@@ -5,9 +5,11 @@ import it.polimi.se2018.model.Color;
 import it.polimi.se2018.model.event.WindowCardEvent;
 import it.polimi.se2018.view.ui.GUIView;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.StrokeType;
 
 
 public class GUIChoiceController implements GUIControllerIF {
@@ -18,6 +20,22 @@ public class GUIChoiceController implements GUIControllerIF {
     private GUIView guiView;
     private WindowCardEvent event;
 
+    @FXML
+    private Label titleNW;
+    @FXML
+    private Label titleNE;
+    @FXML
+    private Label titleSW;
+    @FXML
+    private Label titleSE;
+    @FXML
+    private Label diffNW;
+    @FXML
+    private Label diffNE;
+    @FXML
+    private Label diffSW;
+    @FXML
+    private Label diffSE;
     @FXML
     private GridPane gridNW;
     @FXML
@@ -31,26 +49,60 @@ public class GUIChoiceController implements GUIControllerIF {
 
     public void setEvent(WindowCardEvent event){
         this.event = event;
-    }
-
-    @FXML
-    public void initialize(){
-        /*
         for (WindowCard w : event.getWindowCards()){
             showCard(w, index);
             index++;
         }
-        */
     }
 
     private void showCard(WindowCard windowCard, int index) {
-        if(index == 0) {
-            for (int i = 0; i < windowCard.getRows(); i++) {
-                for (int j = 0; j < windowCard.getCols(); j++) {
-                    gridNW.add(new Rectangle(R_WIDTH, R_HEIGHT, getMatch(windowCard.getGridCell(i, j).getColor())), i, j);
-                }
+        for (int i = 0; i < windowCard.getRows(); i++) {
+            for (int j = 0; j < windowCard.getCols(); j++) {
+                putInGrid(index, createRect(windowCard, i, j), j, i);
+                putNameAndDiff(windowCard);
             }
         }
+
+    }
+
+    private void putNameAndDiff(WindowCard windowCard) {
+        if(index == 0) {
+            titleNW.setText(windowCard.getWindowName());
+            diffNW.setText(String.valueOf(windowCard.getDifficulty()));
+        }
+        else if(index == 1) {
+            titleNE.setText(windowCard.getWindowName());
+            diffNE.setText(String.valueOf(windowCard.getDifficulty()));
+        }
+        else if(index == 2) {
+            titleSW.setText(windowCard.getWindowName());
+            diffSW.setText(String.valueOf(windowCard.getDifficulty()));
+        }
+        else if(index == 3){
+            titleSE.setText(windowCard.getWindowName());
+            diffSE.setText(String.valueOf(windowCard.getDifficulty()));
+        }
+    }
+
+    private void putInGrid(int index, Rectangle rect, int j, int i) {
+        if(index == 0)
+            gridNW.add(rect, j, i);
+        else if(index == 1)
+            gridNE.add(rect, j, i);
+        else if(index == 2)
+            gridSW.add(rect, j, i);
+        else if(index == 3)
+            gridSE.add(rect, j, i);
+
+    }
+
+    private Rectangle createRect(WindowCard windowCard, int i, int j) {
+        Rectangle rect = new Rectangle(R_WIDTH, R_HEIGHT, getMatch(windowCard.getGridCell(i, j).getColor()));
+        rect.setStrokeType(StrokeType.INSIDE);
+        rect.setStroke(javafx.scene.paint.Color.BLACK);
+        rect.setStrokeWidth(2);
+
+        return rect;
     }
 
     private Paint getMatch(Color c){
